9064026bbc599c46d37d52cbaa8885dae27b1280
ardynjr <freshmeme111@gmail.com>
2025-11-25 20:07:50 +0700

    Update create.php

--- create.php
+++ create.php
@@ -33,7 +33,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tambah Karyawan</title>
+    <title>Tambah Anggota Tim</title>
     <style>
         /* Modern Color Palette (Sama dengan index.php) */
         :root {
